d545fafac1619c83bdeafdefc4fe03e51b7f20f7
Mohamed Ashraf <medoalex350@gmail.com>
2025-06-17 22:10:24 +0300

    feat: Implement Splash Screen with Animation

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,1 @@
+const String kAppLogo = 'assets/nawel.png';
---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xffE3F2FD);
+const Color kSecondaryColor = Color(0xff188B76);
+const Color kTertiaryColor = Color(0xffF5F4E2);
+const Color kTextColor = Colors.black87;
